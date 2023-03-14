01d11179bc43504842baabb0c3b11b84a7002493
tkostadinov004 <tkostadinov04@gmail.com>
2023-03-14 18:46:52 +0200

    Added admin

---
+++ Cinema/Areas/Identity/Pages/Admin/RegisterOwner.cshtml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Cinema.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Cinema.Models.Enums;
+using Cinema.Areas.Identity.Pages.Account;
+
+namespace Cinema.Areas.Identity.Pages.Admin
+{
+    [Authorize(Roles = "Administrator")]
+    public class RegisterOwnerModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
+
+        public RegisterOwnerModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+
+        [BindProperty]
+        public InputOwnerModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        public class InputOwnerModel
+        {
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "Паролата трябва да бъде поне {2} символа и най-много {1} символа.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Парола")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Потвърди парола")]
+            [Compare("Password", ErrorMessage = "Двете пароли не съвпадат.")]
+            public string ConfirmPassword { get; set; }
+
+            [DataType(DataType.Text)]
+            [StringLength(100, ErrorMessage = "Собственото име трябва да бъде поне {2} символа и най-много {1} символа", MinimumLength = 5)]
+            [Display(Name = "Собствено име")]
+            public string FirstName { get; set; }
+            [DataType(DataType.Text)]
+            [StringLength(100, ErrorMessage = "Фамилното име трябва да бъде поне {2} символа и най-много {1} символа", MinimumLength = 5)]
+            [Display(Name = "Фамилно име")]
+            public string LastName { get; set; }
+        }
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName, EmailConfirmed = true };
+                var result = await _userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+                    await _userManager.AddToRoleAsync(user, "Owner");
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
+}
---
+++ Cinema/Controllers/AdminController.cs
@@ -0,0 +1,55 @@
+﻿using Cinema.Data;
+using Cinema.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinema.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdminController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Users.ToListAsync().Result.Where(i => _userManager.IsInRoleAsync(i, "Owner").Result == true));
+        }
+
+        [HttpGet]
+        public IActionResult DeleteOwner(string ownerId)
+        {
+            var owner = _context.Users.ToListAsync().Result.FirstOrDefault(i => i.Id == ownerId &&  _userManager.IsInRoleAsync(i, "Owner").Result == true);
+            if (owner == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(owner);
+        }
+        [HttpPost, ActionName("DeleteOwner")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var owner = _context.Users.ToListAsync().Result.FirstOrDefault(i => i.Id == id);
+            _context.Users.Remove(owner);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
