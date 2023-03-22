2b4c4a71304e51febb79d8cb4e9bbef5166220da
tkostadinov004 <tkostadinov04@gmail.com>
2023-03-22 21:26:25 +0200

    Added movies, actors and seats seeding

---
+++ DbSeeder.Data/DTOs/AddActorMovieDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbSeeder.Data.DTOs
+{
+    public class AddActorMovieDTO
+    {
+        public string Actor { get; set; }
+        public string Movie { get; set; }
+    }
+}
---
+++ DbSeeder.Data/DTOs/AddMovieDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbSeeder.Data.DTOs
+{
+    public class AddMovieDTO
+    {
+        public string EnglishTitle { get; set; }
+        public string BulgarianTitle { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+        public string Price { get; set; }
+        public string RunningTime { get; set; }
+        public string TrailerUrl { get; set; }
+    }
+}
