fc59941dbd5a785d9db74baab94aa93435fe838d
tkostadinov004 <tkostadinov04@gmail.com>
2023-07-03 21:31:30 +0300

    Renamed the date validation attribute

---
+++ Cinema.Extensions/ValidationAttributes/PastDateValidationAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinema.Extensions
+{
+    public class PastDateValidationAttribute : ValidationAttribute
+    {
+        public PastDateValidationAttribute()
+        {
+
+        }
+        public override bool IsValid(object? value)
+        {
+            if (value is DateTime || value is DateTime?)
+            {
+                var date = (DateTime)value;
+                if (date <= DateTime.Now)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+    }
+}
