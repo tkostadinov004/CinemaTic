800fc0d065fe99603900fce1ea527746cbdb2bb5
tkostadinov004 <tkostadinov04@gmail.com>
2023-08-01 15:57:21 +0300

    Removed old files

--- Cinema.Core/Contracts/ILogService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Cinema.Data.Enums;
-
-namespace Cinema.Core.Contracts
-{
-    public interface ILogService
-    {
-        Task LogActionAsync(UserActionType type, string message, params object[] attributes);
-    }
-}
